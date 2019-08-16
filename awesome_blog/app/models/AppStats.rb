7daea37f29f8b49d3f4ff78f9fd5a67c25621da6
root <root@LAPTOP-Q4DLJ7FC.localdomain>
2019-08-16 12:42:32 -0400

    wrote APPStats.rb

--- awesome_blog/app/models/AppStats.rb
+++ awesome_blog/app/models/AppStats.rb
@@ -1,4 +1,36 @@
 class AppStats
-    def initialize()
+    attr_reader :posts :quotes
+    def initialize(posts, quotes)
+        @posts = posts
+        @quotes = quotes
+    end
+    def posts_count
+        @posts.length
+    end
+    def quotes_count
+        @quotes.length
+    end
+    def posts_word_count
+        words = 0
+        count = @posts
+        count.array.map{ |i| words = words + i.body.split.size}
+        return words
+    end
+    def quotes_word_count
+        words = 0
+        count = @quotes
+        count.array.map{ |i| words = words + i.body.split.size}
+        return words
+    end
+    def total_count
+        a = posts_count
+        b = quotes_count
+        return a + b
+    end
+    def total_word_count
+        a = posts_word_count
+        b = quotes_word_count
+        return a + b
+
     
 end
